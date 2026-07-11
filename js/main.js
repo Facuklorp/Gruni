3840c0e71501390f6573043ca271795a3bc04838
Facundo <facundo@gruni.local>
2026-07-11 04:13:26 -0300

    Implementar Fondo nuevo.png y restaurar entidades (ignorar PSDs)

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@ const imageUrls = {
     bg_pasto: 'Biomas/GRANDES/Pasto.png',
     bg_agua: 'Biomas/GRANDES/Agua.png',
     bg_arena: 'Biomas/GRANDES/Arena.png',
-    fondo_gruni: 'Fondogruni.png'
+    fondo_gruni: 'Fondo nuevo.png'
 };
 
 function loadImage(url) {
